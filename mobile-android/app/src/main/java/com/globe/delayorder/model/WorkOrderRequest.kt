db9d662d2c505c531345b3e3e69c73ac9f071251
jedebisu <jed.ebisu@gmail.com>
2026-09-06 09:34:57 +0800

    Update Kotlin data model fields to match API JSON response

--- mobile-android/app/src/main/java/com/globe/delayorder/model/WorkOrderRequest.kt
+++ mobile-android/app/src/main/java/com/globe/delayorder/model/WorkOrderRequest.kt
@@ -2,19 +2,19 @@ package com.globe.delayorder.model
 
 data class WorkOrderRequest(
     val id: Int,
-    val timestamp: String,
-    val work_order_number: String,
-    val partner: String,
-    val channel: String,
-    val delay_info: String?,
-    val delay_date_and_time: String?,
-    val with_3_way_notes: Boolean,
-    val person_3_wayed: String?,
-    val three_way_notes: String?,
-    val status: String,
-    val bde_remarks: String?,
-    val approver_email: String?,
-    val action_taken_at: String?
+    val timestamp: String?,
+    val workordernumber: String?,
+    val partner: String?,
+    val channel: String?,
+    val delay: String?,
+    val delaydateandtime: String?,
+    val with3waynotes: Boolean?,
+    val person3wayed: String?,
+    val threewaynotes: String?,
+    val status: String?,
+    val bderemarks: String?,
+    val approverEmail: String?,
+    val actionTakenAt: String?
 )
 
 data class ApproverActionRequest(
