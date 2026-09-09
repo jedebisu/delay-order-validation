7c12e1e7be7b1f7bd238441b66e8a0c64001c8aa
jedebisu <jed.ebisu@gmail.com>
2026-09-09 20:07:20 +0800

    Bump to v2.4 (versionCode 6) for timestamp fix release

--- mobile-android/app/build.gradle.kts
+++ mobile-android/app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.globe.delayorder"
         minSdk = 24
         targetSdk = 34
-        versionCode = 5
-        versionName = "2.3"
+        versionCode = 6
+        versionName = "2.4"
     }
 
     compileOptions {
